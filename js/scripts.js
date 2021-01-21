bdda0e9909777bce7c871e521ab6489fb4fb234f
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-21 14:53:55 -0800

    convert border cases v.2-we won!
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email.gmail.com>
    Co-authored-by: Andrew Lawson <alawson89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,52 +15,20 @@ function romanNumerals(input) {
     let num = Math.trunc(input / parameters[i]);
     input = (input % parameters[i]);
     let ret = convert(num, romNum[i]);
-    if ((ret.length > 3) && (i !== 0)) {
-      sentence.push(romNum[i])
-      sentence.push(romNum[i - 1]);
+    if ((ret.length > 3) && (i !== 0) && (sentence[i - 1] === romNum[i - 1])) {
+      sentence.pop();
+      sentence.push(romNum[i]);
+      sentence.push(romNum[i - 2]);
+    } else if ((ret.length > 3) && (i !== 0)) {
+      sentence.push(romNum[i] + romNum[i - 1]);
     } else {
       sentence.push(ret);
     }
   }
 
   return sentence.join('');
-
-
-
-  /*let rm = (input % 1000);
-  input = Math.trunc(input / 1000);
-  const m1 = convert(m, 'M');
-  sentence.push(m1);
-
-  const d = Math.trunc(rm / 500);
-  const d1 = convert(d, 'D');
-  sentence.push(d1);
-  let rm2 = (rm % 500);
-
-  const c = Math.trunc(rm2 / 100);
-  const c1 = convert(c, 'C');
-  sentence.push(c1);
-  let rm3 = (rm2 % 100);
-
-  const l = Math.trunc(rm3 / 50);
-  const l1 = convert(l, 'L');
-  sentence.push(l1);
-  let rm4 = (rm3 % 50);
-
-  const x = Math.trunc(rm4 / 10);
-  const x1 = convert(x, 'X');
-  sentence.push(x1);
-  let rm5 = (rm4 % 10);
-
-  const v = Math.trunc(rm5 / 5);
-  const v1 = convert(v, 'V');
-
-  sentence.push(v1);
-  let i = (rm5 % 5);
-  const i1 = convert(i, 'I');
-  sentence.push(i1);*/
-
 }
+
 
 $(document).ready(function() {
   $('form').submit(function() {
