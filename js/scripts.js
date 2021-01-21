a601cf2d9f969cc556e0816c1d2cb815660f98c6
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-21 13:41:01 -0800

    pass all tests and display result for a user
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email.gmail.com>
    Co-authored-by: Andrew Lawson <alawson89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,45 @@
 function convert(number, letter) {
-  let sentence = [];
+  let array = [];
   for (let i = 0; i < number; i++) {
-    sentence.push(letter);
-    return sentence;
+    array.push(letter);
   }
+  return array.join('');
 }
 
 function romanNumerals(input) {
   let sentence = [];
   const m = Math.trunc(input / 1000);
-  for (let i = 0; i < m; i++) {
-    sentence.push('M');
-  }
+  const m1 = convert(m, 'M');
+  sentence.push(m1);
+
   let rm = (input % 1000);
   const d = Math.trunc(rm / 500);
-  for (let i = 0; i < d; i++) {
-    sentence.push('D');
-  }
+  const d1 = convert(d, 'D');
+  sentence.push(d1);
   let rm2 = (rm % 500);
+
   const c = Math.trunc(rm2 / 100);
-  for (let i = 0; i < c; i++) {
-    sentence.push('C');
-  }
-  let rm3 = (c % 100);
+  const c1 = convert(c, 'C');
+  sentence.push(c1);
+  let rm3 = (rm2 % 100);
+
   const l = Math.trunc(rm3 / 50);
-  for (let i = 0; i < l; i++) {
-    sentence.push('L');
-  }
+  const l1 = convert(l, 'L');
+  sentence.push(l1);
   let rm4 = (rm3 % 50);
+
   const x = Math.trunc(rm4 / 10);
-
+  const x1 = convert(x, 'X');
+  sentence.push(x1);
   let rm5 = (rm4 % 10);
+
   const v = Math.trunc(rm5 / 5);
+  const v1 = convert(v, 'V');
+
+  sentence.push(v1);
   let i = (rm5 % 5);
-
-
+  const i1 = convert(i, 'I');
+  sentence.push(i1);
 
   return sentence.join('');
 }
@@ -44,5 +49,6 @@ $(document).ready(function() {
     event.preventDefault();
     const input = parseInt($('#user').val());
     const out = romanNumerals(input);
+    $('.output').text(out);
   });
 })
